0e3a81cc7b440db8ee388d975235e3a92fb08619
riyapahuja20 <riyapahuja20@yahoo.com>
2018-06-08 18:02:01 +0530

    Made changes to parse json request

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 
 const port = 8000;
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json({extended:true}));
 
 MongoClient.connect(db.url, (err, database) => {
     if (err) return console.log(err)
